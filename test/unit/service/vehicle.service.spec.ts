f6e50c1496582acb4ca7fff0cc8295fce573fc90
Paulo da Luz <paulo.luz@compasso.com.br>
2021-08-27 23:40:28 -0300

    feat: creating unit tests to vehicles

--- test/unit/service/vehicle.service.spec.ts
+++ test/unit/service/vehicle.service.spec.ts
@@ -14,7 +14,7 @@ describe('VehicleService test', () => {
 
     vehicleRepository.getVehicleByRegistrationPlate = jest
       .fn()
-      .mockResolvedValueOnce(MockData.vehicle);
+      .mockResolvedValueOnce(MockData.vehicleResponse);
 
     const result = await vehicleService.getVehicleInfo('IBMC2789');
 
@@ -34,7 +34,7 @@ describe('VehicleService test', () => {
   });
 
   it('should return a vehicle by cache on operation getVehicleInfo', async () => {
-    cacheRepository.getFromCache = jest.fn().mockResolvedValueOnce(MockData.vehicle);
+    cacheRepository.getFromCache = jest.fn().mockResolvedValueOnce(MockData.vehicleResponse);
 
     const result = await vehicleService.getVehicleInfo('IBMC2789');
 
@@ -44,6 +44,91 @@ describe('VehicleService test', () => {
     expect(Array.isArray(result.busStations)).toBe(true);
     expect(result.busStations.length).toBe(1);
     expect(result.passwordToShareLocalization).toEqual('MH0M7FXMF');
+    expect(result.passwordToShareLocalization?.length).toEqual(9);
+    expect(result.priceTransport).toEqual(3.9);
+    expect(result.isPublic).toEqual(false);
+    expect(result.airConditioning).toEqual(true);
+    expect(result.suportWheelchair).toBe(true);
+    expect(result.washrooms).toBe(true);
+    expect(result.wifi).toBe(true);
+  });
+
+  it('should return a error on operation getVehicleInfo', async () => {
+    cacheRepository.getFromCache = jest.fn().mockResolvedValueOnce(undefined);
+
+    try {
+      await vehicleService.getVehicleInfo('IBMC2789');
+    } catch (err) {
+      expect(err.status).toEqual(404);
+      expect(err.message).toEqual('The specified resource is not found.');
+    }
+  });
+
+  it('should return success on creating a vehicle', async () => {
+    cacheRepository.getFromCache = jest.fn().mockResolvedValueOnce(undefined);
+
+    vehicleRepository.getVehicleByRegistrationPlate = jest.fn().mockResolvedValueOnce(undefined);
+
+    const result = await vehicleService.createVehicle(MockData.vehicleToCreate);
+
+    expect(result.registrationPlate).toEqual('IBMC2789');
+    expect(result.name).toEqual('Hipica Planaltina');
+    expect(result.idToPassengers).toEqual('#WZ6WBI');
+    expect(Array.isArray(result.busStations)).toBe(true);
+    expect(result.busStations.length).toBe(1);
+    expect(result.passwordToShareLocalization?.length).toEqual(9);
+    expect(result.priceTransport).toEqual(3.9);
+    expect(result.isPublic).toEqual(false);
+    expect(result.airConditioning).toEqual(true);
+    expect(result.suportWheelchair).toBe(true);
+    expect(result.washrooms).toBe(true);
+    expect(result.wifi).toBe(true);
+  });
+
+  it('should return error on creating a vehicle because exists on cache', async () => {
+    cacheRepository.getFromCache = jest.fn().mockResolvedValueOnce(MockData.vehicleResponse);
+
+    try {
+      await vehicleService.createVehicle(MockData.vehicleToCreate);
+    } catch (err) {
+      expect(err.status).toEqual(400);
+      expect(err.message).toEqual(
+        'Client specified an invalid argument, request body or query param.',
+      );
+    }
+  });
+
+  it('should return error on creating a vehicle because vehicle already exists', async () => {
+    cacheRepository.getFromCache = jest.fn().mockResolvedValueOnce(undefined);
+
+    vehicleRepository.getVehicleByRegistrationPlate = jest
+      .fn()
+      .mockResolvedValueOnce(MockData.vehicleResponse);
+
+    try {
+      await vehicleService.createVehicle(MockData.vehicleToCreate);
+    } catch (err) {
+      expect(err.status).toEqual(400);
+      expect(err.message).toEqual(
+        'Client specified an invalid argument, request body or query param.',
+      );
+    }
+  });
+
+  it('should return success on updating a vehicle', async () => {
+    vehicleRepository.updateVehicleByRegistrationPlate = jest.fn().mockImplementation();
+
+    vehicleRepository.getVehicleByRegistrationPlate = jest
+      .fn()
+      .mockResolvedValueOnce(MockData.vehicleResponse);
+
+    const result = await vehicleService.updateVehicle('IBMC2789', MockData.vehicleResponse);
+
+    expect(result.registrationPlate).toEqual('IBMC2789');
+    expect(result.name).toEqual('Hipica Planaltina');
+    expect(result.idToPassengers).toEqual('#WZ6WBI');
+    expect(Array.isArray(result.busStations)).toBe(true);
+    expect(result.busStations.length).toBe(1);
     expect(result.passwordToShareLocalization?.length).toEqual(9);
     expect(result.priceTransport).toEqual(3.9);
     expect(result.isPublic).toEqual(false);
