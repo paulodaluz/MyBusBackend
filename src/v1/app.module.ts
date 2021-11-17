6e5da70b39d737684a1013c18af7266f7ab7e642
Paulo da Luz <paulo.daluzjr@gmail.com>
2021-11-16 22:47:24 -0300

    feat: removing redis by module

--- src/v1/app.module.ts
+++ src/v1/app.module.ts
@@ -1,10 +1,8 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import * as redisStore from 'cache-manager-redis-store';
 import { FeedbackController } from './controllers/feedback.controller';
 import { UserController } from './controllers/user.controller';
 import { VehicleController } from './controllers/vehicle.controller';
-import { CacheRepository } from './repository/cache.repository';
 import { FeedbackRepository } from './repository/feedback.repository';
 import { UserRepository } from './repository/user.repository';
 import { VehicleRepository } from './repository/vehicle.repository';
@@ -12,17 +10,10 @@ import { FeedbackService } from './services/feedback.service';
 import { UserService } from './services/user.service';
 import { VehicleService } from './services/vehicle.service';
 
-const cacheDatabase: any = process.env.REDIS_ENABLE === 'false' ? 'memory' : redisStore;
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
-    }),
-    CacheModule.register({
-      store: cacheDatabase,
-      host: process.env.REDIS_HOST,
-      port: Number(process.env.REDIS_PORT),
     }),
   ],
   controllers: [VehicleController, UserController, FeedbackController],
@@ -30,7 +21,6 @@ const cacheDatabase: any = process.env.REDIS_ENABLE === 'false' ? 'memory' : red
     VehicleService,
     UserService,
     VehicleRepository,
-    CacheRepository,
     UserRepository,
     FeedbackService,
     FeedbackRepository,
